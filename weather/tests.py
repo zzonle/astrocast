b835ce28b73c589e2a27adf9ddb06d25383973c4
Gerald Knopke <gerald.knopke@inacapmail.cl>
2025-11-24 21:45:34 -0300

    aa

--- weather/tests.py
+++ weather/tests.py
@@ -1,3 +1,4 @@
 from django.test import TestCase
 
 # Create your tests here.
+#wather logre recibir autenticacion de json web token  y guarde los datos de la consulta, guarde quien la hizo, la consulta y fecha.
